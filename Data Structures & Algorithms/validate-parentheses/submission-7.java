3252b4661df655ae62d9dc5210439644ed355342
Somee-15 <heart4every1a@gmail.com>
2026-09-24 02:00:53 +0530

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+
+        ArrayDeque<Character> stack = new ArrayDeque();
+
+        int i=0;
+
+        while(i<s.length()){
+            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
+                stack.push(s.charAt(i));
+            }else{
+               
+                Character top = stack.peek();                
+                if( null!=top && ((top=='[' && s.charAt(i)==']') || (top=='(' && s.charAt(i)==')')
+                || (top=='{' && s.charAt(i)=='}'))){
+                    stack.poll();
+                }else{
+                     stack.push(s.charAt(i));
+                }
+            }
+            i++;
+        }
+
+        return stack.isEmpty();
+    }
+}
